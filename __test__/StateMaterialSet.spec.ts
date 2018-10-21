01e36809ea7d7ec2a92305644303b5f178e7be5b
MasatoMakino <unagiinu.g@gmail.com>
2018-10-21 15:22:10 +0900

    配列型のStateMaterialのテストを追加。

---
+++ __test__/StateMaterialSet.spec.ts
@@ -0,0 +1,56 @@
+import { StateMaterial } from "../src/index";
+import { MeshBasicMaterial, MeshMaterialType } from "three";
+
+describe("StateMaterial", () => {
+  let mat: StateMaterial;
+  let matArray: MeshMaterialType[];
+
+  test("constructor : Material array", () => {
+    matArray = [
+      new MeshBasicMaterial({
+        color: 0xffffff,
+        opacity: 0.6,
+        transparent: true
+      }),
+      new MeshBasicMaterial({
+        color: 0xffffff,
+        opacity: 0.5,
+        transparent: true
+      }),
+      new MeshBasicMaterial({
+        color: 0xffffff,
+        opacity: 0.4,
+        transparent: true
+      }),
+      new MeshBasicMaterial({
+        color: 0xffffff,
+        opacity: 0.3,
+        transparent: true
+      }),
+      new MeshBasicMaterial({
+        color: 0xffffff,
+        opacity: 0.2,
+        transparent: true
+      }),
+      new MeshBasicMaterial({
+        color: 0xffffff,
+        opacity: 0.1,
+        transparent: true
+      })
+    ];
+    mat = new StateMaterial(matArray);
+
+    expect((mat.material as MeshMaterialType[])[0].opacity).toBe(0.6);
+  });
+
+  test("updateAlpha : Material array", () => {
+    mat.setOpacity(0.5);
+    const array = mat.material as MeshMaterialType[];
+    expect(array[0].opacity).toBe(0.3);
+    expect(array[1].opacity).toBe(0.25);
+    expect(array[2].opacity).toBe(0.2);
+    expect(array[3].opacity).toBe(0.15);
+    expect(array[4].opacity).toBe(0.1);
+    expect(array[5].opacity).toBe(0.05);
+  });
+});
