ae6a8a024e925cf950cb42fc8f7e5b2c69582341
MasatoMakino <unagiinu.g@gmail.com>
2019-05-27 13:29:29 +0900

    serverタスクをgulpに入れ替え

---
+++ gulpfile.js/index.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const { series } = require("gulp");
+
+// const doc = require("gulptask-tsdoc")();
+const server = require("gulptask-dev-server")("./docs");
+exports.server = server;
+// const { bundleDevelopment, watchBundle } = require("gulptask-webpack")(
+//   "./webpack.config.js"
+// );
+// const { tsc, watchTsc } = require("gulptask-tsc")();
+//
+// const watchTasks = cb => {
+//   watchBundle();
+//   watchTsc();
+//   cb();
+// };
+//
+// exports.start_dev = series(watchTasks, server);
+// exports.build = series(tsc, bundleDevelopment, doc);
