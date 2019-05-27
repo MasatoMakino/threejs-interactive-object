beb21fac4ef16f527fea0751ae520f5b52113ee2
MasatoMakino <unagiinu.g@gmail.com>
2019-05-27 13:35:51 +0900

    typedocタスクをgulpに入れ替え。

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -2,7 +2,8 @@
 
 const { series } = require("gulp");
 
-// const doc = require("gulptask-tsdoc")();
+const doc = require("gulptask-tsdoc")();
+exports.doc = doc;
 const server = require("gulptask-dev-server")("./docs");
 exports.server = server;
 // const { bundleDevelopment, watchBundle } = require("gulptask-webpack")(
