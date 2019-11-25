499aa744ac58fe123f4b6c01ba014864ea7a8057
MasatoMakino <unagiinu.g@gmail.com>
2019-11-25 22:22:51 +0900

    change : ts-jest config. WIP.

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   preset: "ts-jest",
-  collectCoverageFrom: [
-    "src/**/*.{js,ts}",
-    "!<rootDir>/__test__/",
-    "!<rootDir>/docs/api/",
-    "!<rootDir>/task/",
-  ]
+  testEnvironment: "node",
+  globals: {
+    "ts-jest": {
+      tsConfig: "tsconfig.test.json"
+    }
+  }
 };
