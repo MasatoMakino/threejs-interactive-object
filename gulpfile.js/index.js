7000429ac6963d09be39212ff744386168555f6d
MasatoMakino <unagiinu.g@gmail.com>
2020-01-06 17:35:48 +0900

    change : build task, parallel

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const { dest, series, src, watch } = require("gulp");
+const { parallel, series } = require("gulp");
 
 const doc = require("gulptask-tsdoc").get();
 const server = require("gulptask-dev-server").get("./docs/demo");
@@ -18,5 +18,5 @@ const watchTasks = async () => {
 };
 
 exports.start_dev = series(watchTasks, server);
-exports.build = series(tsc, bundleDemo, doc);
-exports.build_clean = series(tscClean, bundleDemo, doc);
+exports.build = series(tsc, parallel(bundleDemo, doc));
+exports.build_clean = series(tscClean, parallel(bundleDemo, doc));
