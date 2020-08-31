6c43af759700c9b77ae2ea326f15dd822abe9123
MasatoMakino <unagiinu.g@gmail.com>
2020-08-31 12:10:18 +0900

    add : test for overlapping buttons

--- __test__/MouseEventManager.spec.ts
+++ __test__/MouseEventManager.spec.ts
@@ -6,6 +6,11 @@ describe("MouseEventManager", () => {
   const managerScene = new MouseEventManagerScene();
   const btn = new MouseEventManagerButton();
   managerScene.scene.add(btn.button);
+
+  const btnBackground = new MouseEventManagerButton();
+  btnBackground.button.position.setZ(-10);
+  managerScene.scene.add(btnBackground.button);
+
   const halfW = managerScene.canvas.width / 2;
   const halfH = managerScene.canvas.height / 2;
 
@@ -23,19 +28,23 @@ describe("MouseEventManager", () => {
     managerScene.render();
     //スロットリングされるのでnormalのまま
     btn.checkMaterial(ClickableState.NORMAL);
+    btnBackground.checkMaterial(ClickableState.NORMAL);
 
     //スロットリングされるのでnormalのまま
     managerScene.interval(0.1);
     managerScene.dispatchMouseEvent("mousemove", halfW, halfH);
     btn.checkMaterial(ClickableState.NORMAL);
+    btnBackground.checkMaterial(ClickableState.NORMAL);
 
     managerScene.interval();
     managerScene.dispatchMouseEvent("mousemove", halfW, halfH);
     btn.checkMaterial(ClickableState.OVER);
+    btnBackground.checkMaterial(ClickableState.NORMAL);
 
     managerScene.interval();
     managerScene.dispatchMouseEvent("mousemove", 0, 0);
     btn.checkMaterial(ClickableState.NORMAL);
+    btnBackground.checkMaterial(ClickableState.NORMAL);
 
     managerScene.reset();
   });
@@ -46,8 +55,10 @@ describe("MouseEventManager", () => {
 
     managerScene.dispatchMouseEvent("mousedown", halfW, halfH);
     btn.checkMaterial(ClickableState.DOWN);
+    btnBackground.checkMaterial(ClickableState.NORMAL);
 
     managerScene.dispatchMouseEvent("mouseup", halfW, halfH);
     btn.checkMaterial(ClickableState.NORMAL);
+    btnBackground.checkMaterial(ClickableState.NORMAL);
   });
 });
